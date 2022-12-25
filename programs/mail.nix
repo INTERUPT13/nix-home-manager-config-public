89e31badad914a4da2941d09cdcdf239dfbd1f9a
flandre <flandre@no.where>
2022-12-25 19:42:57 +0000

    mail.nix

---
+++ programs/mail.nix
@@ -0,0 +1,23 @@
+{nix-home-manager-config-secrets,...}: {
+        programs.himalaya = {
+          enable = true;
+        };
+
+        # each account needs a imap ={host,port,tls.enable}; and smtp={}; 
+        # userName="", realName="", address="sth@domain.tld", and passwordCommand="";
+        # i will keep these in a private config to not only avoid spam but also
+        # for privacy reason. It kind of does not complain at the moment if you
+        # are missing stuff so make sure you have everything configured and did not forget sth!!
+        accounts.email.accounts = {
+          r = {
+            primary = true;
+            himalaya = {
+              enable = true;
+              backend = "imap";
+              sender = "smtp"; 
+              #sender = "sendmail"; #if used on the mailserver itself?
+
+            };
+          } // import ("${nix-home-manager-config-secrets}/mail/creds_r.nix");
+        };
+}
