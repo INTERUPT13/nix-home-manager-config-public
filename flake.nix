99822fc3ab8dd1ce53ce5c6842c6e472de2062c9
flandre <flandre@no.where>
2023-01-27 15:57:59 +0000

    don't re-fetch nixpkgs

--- flake.nix
+++ flake.nix
@@ -51,26 +51,26 @@
       # to be used with the nixos home-manager module + standalone home-manager
       # TODO make this one desktop the default one shall be more lightweight and
       # more aimed at useful sysadmin tools + nvim
-      pinephone_cfg = with phonepkgs; {
+      pinephone_cfg = pkgs: {
         home = {
           stateVersion = "22.11";
 
           # TODO secrets management for every program that needs secrets
-          packages = with phonepkgs;
+          packages = with pkgs;
             [
               tdesktop # sinke kotatogram or how its called seems to not run on aarch or whatever
 
-            ] ++ (import ./packages/multimedia.nix (phonepkgs))
+            ] ++ (import ./packages/multimedia.nix (pkgs))
             # ++ (import ./packages/social.nix (pkgs)) # as for now kotatogram is x86 only make a config that does a match case on pkgs.system
             #++ (import ./packages/web.nix (pkgs))
             #++ (import ./packages/crypto.nix (pkgs))
-            ++ (import ./packages/linux_tools.nix (phonepkgs))
-            ++ (import ./packages/nix_tools.nix (phonepkgs))
+            ++ (import ./packages/linux_tools.nix (pkgs))
+            ++ (import ./packages/nix_tools.nix (pkgs))
             #++ (import ./packages/binary_debugging.nix (pkgs))
             #TODO make "rr" free version for aarch64 ++ (import ./packages/security.nix (phonepkgs))
-            ++ (import ./packages/terminal.nix (phonepkgs))
-            ++ (import ./packages/doc_creation.nix (phonepkgs))
-            ++ (import ./packages/remote_access.nix (phonepkgs));
+            ++ (import ./packages/terminal.nix (pkgs))
+            ++ (import ./packages/doc_creation.nix (pkgs))
+            ++ (import ./packages/remote_access.nix (pkgs));
         };
 
 
